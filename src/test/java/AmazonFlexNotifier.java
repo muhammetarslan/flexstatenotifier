6218ee58d64a29e22ece1a7c368fcc380ded9196
muhammetarslan <artemis1940@gmail.com>
2020-04-28 13:01:14 -0400

    red

---
+++ src/test/java/AmazonFlexNotifier.java
@@ -0,0 +1,25 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AmazonFlexNotifier {
+
+    @Test
+    public void notifier(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://flex.amazon.com/get-started");
+        List<WebElement> divs=driver.findElements(By.xpath("//div[@class='container  padding-top-xlarge swa background-color-transparent']//div"));
+        List<String> texts=divs.stream().map(x->x.getText()).collect(Collectors.toList());
+        Assert.assertFalse(texts.contains("Georgia"));
+        System.out.println(texts);
+        driver.quit();
+    }
+}
